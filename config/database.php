8ff4ee05b791c3148268f4e994913d739e10acfe
EvilDemo127 <maungzawshein20@gmail.com>
2026-07-10 15:08:22 +0700

    prepare host in vercel

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' =>  'pgsql',
 
     /*
     |--------------------------------------------------------------------------
@@ -97,6 +97,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+             'options' => [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
